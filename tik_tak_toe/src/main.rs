95b18b428a1f58c2266f3072c457456086195f80
cid <ciditworld@gmail.com>
2022-04-03 00:13:57 -0400

    added projects for tik tak toe and variables

---
+++ tik_tak_toe/src/main.rs
@@ -0,0 +1,13 @@
+
+
+
+
+fn main() {
+    println!("Tik Tak Toe!");
+
+    let places = "{}{}{}\
+                  {}{}{}\
+                  {}{}{}";
+    println!("{}{}{}\n{}{}{}\n{}{}{}\n", "x", "o", "x", "o", "x", "o", "x", "o", "x");
+    
+}
