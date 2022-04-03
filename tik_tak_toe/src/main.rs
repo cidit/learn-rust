906e2d0459b3a4b8cac91a4f1a09541cfe7365c6
cid <ciditworld@gmail.com>
2022-04-03 02:06:47 -0400

    started work on the tik tak toe. displaying the state of the game is done for now.

--- tik_tak_toe/src/main.rs
+++ tik_tak_toe/src/main.rs
@@ -1,13 +1,36 @@
+#[derive(Debug)]
+enum PlayerSymbol {
+    X,
+    O,
+}
 
-
-
+const MAX_SCALE: i32 = 9;
 
 fn main() {
     println!("Tik Tak Toe!");
 
-    let places = "{}{}{}\
-                  {}{}{}\
-                  {}{}{}";
-    println!("{}{}{}\n{}{}{}\n{}{}{}\n", "x", "o", "x", "o", "x", "o", "x", "o", "x");
-    
+    let scale = 3;
+    let alphabet: Vec<_> = "abcdefghijklmnopqrstuvwxyz".chars().collect();
+    let places: Vec<Option<PlayerSymbol>> = (0..scale * scale).map(|_| None).collect();
+
+    println!(
+        " {}",
+        (1..scale + 1).map(|x| x.to_string()).reduce(|a, b| format!("{}{}", a, b)).unwrap()
+    );
+
+
+    for x in 0..scale {
+        print!("{}", alphabet[x]);
+        for y in 0..scale {
+            print!(
+                "{}",
+                match &places[x * y] {
+                    Some(PlayerSymbol::X) => "x",
+                    Some(PlayerSymbol::O) => "o",
+                    None => ".",
+                }
+            )
+        }
+        println!()
+    }
 }
