0b67fd2da58615df718e19a1116492edacca2a94
cid <ciditworld@gmail.com>
2022-04-04 13:51:55 -0400

    refactored data types, finished implementing who_wins. needs testing.

--- tik_tak_toe/src/main.rs
+++ tik_tak_toe/src/main.rs
@@ -3,47 +3,75 @@ use std::io::stdin;
 const MAX_SCALE: i32 = 9;
 const DEFAULT_SCALE: i32 = 3;
 
-#[derive(Debug)]
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
 enum PlayerSymbol {
     X,
     O,
 }
 
+type GameState = Vec<Option<PlayerSymbol>>;
+
+struct Game {
+    state: GameState,
+    scale: usize,
+}
+
 fn main() {
     println!("Tik Tak Toe!");
 
-    'main: loop {
-
+    loop {
         let scale = get_game_scale();
 
         game_main(scale as usize);
 
-
-        'exit: loop {
-            println!("new game? [y/n]");
-            let mut input = String::new();
-            stdin().read_line(&mut input).expect("fuck");
-            match input.trim().get(0..1) {
-                Some(input) => if "n".to_owned() == input { break 'main } else { continue 'main},
-                None => continue 'exit,
-            }
+        if !get_continue_game() {
+            break;
         }
     }
-
 }
 
 fn get_game_scale() -> i32 {
     loop {
-        println!("which scale do you want to play at? [max: {}, default: {}]", MAX_SCALE, DEFAULT_SCALE);
+        println!(
+            "which scale do you want to play at? [max: {}, default: {}]",
+            MAX_SCALE, DEFAULT_SCALE
+        );
         let mut input = String::new();
         stdin().read_line(&mut input).expect("fuck");
         match input.trim() {
             "" => return DEFAULT_SCALE,
             other => match other.parse::<i32>() {
-                Ok(it) => if it <= MAX_SCALE && it >= 1 { return it } else { continue },
+                Ok(it) => {
+                    if it <= MAX_SCALE && it >= 3 {
+                        return it;
+                    } else {
+                        continue;
+                    }
+                }
                 Err(_) => continue,
             },
         };
+    }
+}
+
+fn get_continue_game() -> bool {
+    // FIXME: not taking into account anything other than "n" or ""
+    // TODO: add "yesn't" easter egg?
+
+    loop {
+        println!("new game? [y/n, default: y]");
+        let mut input = String::new();
+        stdin().read_line(&mut input).expect("fuck");
+        if input.trim() == "yesn't" {
+            println!("which is it god damn it");
+            continue;
+        }
+        match input.trim().get(0..1) {
+            Some(input) if input == "y" => return true,
+            Some(input) if input == "n" => return false,
+            Some(_) => continue,
+            None => return true,
+        }
     }
 }
 
@@ -51,13 +79,22 @@ fn game_main(scale: usize) {
     // TODO: unfinshed
     println!("new game!");
 
-    let places: Vec<Option<PlayerSymbol>> = (0..scale * scale).map(|_| None).collect();
+    let game = Game {
+        scale,
+        state: (0..scale * scale).map(|_| None).collect(),
+    };
 
-    loop {}
+    loop {
+        println!("Game state:");
+        print_game(&game);
+
+        who_wins(&game);
+        loop {}
+    }
 }
 
-fn print_game(scale: usize, game_state: Vec<Option<PlayerSymbol>>) {
-
+fn print_game(game: &Game) {
+    let scale: usize = game.scale;
     let alphabet: Vec<char> = "abcdefghijklmnopqrstuvwxyz".chars().collect();
 
     println!(
@@ -72,7 +109,7 @@ fn print_game(scale: usize, game_state: Vec<Option<PlayerSymbol>>) {
         for y in 0..scale {
             print!(
                 "{}",
-                match &game_state[x * y] {
+                match game.state[x * y] {
                     Some(PlayerSymbol::X) => "x",
                     Some(PlayerSymbol::O) => "o",
                     None => ".",
@@ -81,4 +118,69 @@ fn print_game(scale: usize, game_state: Vec<Option<PlayerSymbol>>) {
         }
         println!()
     }
+}
+
+// FIXME: try to remove the ref on the Option<&PlayerSymbol>
+// TODO: test that shit
+fn who_wins(game: &Game) -> Option<PlayerSymbol> {
+    fn all_same<T>(v: &Vec<T>) -> bool
+    where
+        T: PartialEq,
+    {
+        return v.windows(2).all(|w| w[0] == w[1]);
+    }
+
+    let first_diagonal: Vec<_> = (0..game.scale)
+        .map(|it| game.scale * it + it)
+        .flat_map(|it| game.state.get(it))
+        .cloned()
+        .collect();
+
+    let second_diagonal: Vec<_> = (0..game.scale)
+        .rev()
+        .map(|it| it + 1)
+        .map(|it| it * game.scale - it)
+        .flat_map(|it| game.state.get(it))
+        .cloned()
+        .collect();
+
+    let rows: Vec<Vec<_>> = (0..game.scale)
+        .map(|x| {
+            return (0..game.scale)
+                .map(|y| y + game.scale * x)
+                .flat_map(|it| game.state.get(it))
+                .cloned()
+                .collect();
+        })
+        .collect();
+
+    let columns: Vec<_> = (0..game.scale)
+        .map(|x| {
+            return (0..game.scale)
+                .map(|y| y * game.scale + x)
+                .flat_map(|it| game.state.get(it))
+                .cloned()
+                .collect();
+        })
+        .collect();
+
+    let mut possible_win_configurations: Vec<Vec<Option<PlayerSymbol>>> = Vec::new();
+    possible_win_configurations.push(first_diagonal);
+    possible_win_configurations.push(second_diagonal);
+    possible_win_configurations.extend(rows);
+    possible_win_configurations.extend(columns);
+
+    println!("{:?}", possible_win_configurations);
+
+    // kind of ugly shit
+    for configuration in possible_win_configurations {
+        if configuration.iter().any(|it| it.is_none()) {
+            continue;
+        }
+        if all_same(&configuration.iter().flatten().collect::<Vec<_>>()) {
+            return configuration[0];
+        }
+    }
+
+    return None;
 }
