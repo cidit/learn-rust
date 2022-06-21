099ed45d9c6dbfaea0504bac4437abdd331ddcbe
cidit <44065673+cidit@users.noreply.github.com>
2022-06-21 17:11:41 -0400

    Added function signatures for basic functionality

--- calculator/src/lib.rs
+++ calculator/src/lib.rs
@@ -1,1 +1,9 @@
 pub mod model;
+
+pub fn parse(input: &srt) -> Vec<model::Tokens> {
+    unimplemented!();
+}
+
+pub fn evaluate(tokens: Vec<model::Tokens>) -> Result<Number, Err> {
+    unimplemented!();
+}
